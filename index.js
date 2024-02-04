4702f40fe768bed663ced32c5e632558f8742193
zainab99a <amanhal729@gmail.com>
2024-02-04 14:05:30 +0300

    complete task three

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 var myName="Zainab"
 var myAge=24
 var isStudent=true
-variableToCheck=false
+var variableToCheck=false
+var name="Zainab"
+var greeting="Hello"
+
 console.log("My name is ",myName)
 console.log("My age is ",myAge)
 console.log("I'm student in SCT ",isStudent)
 //task 2
 console.log("Data type of variable is",typeof variableToCheck)
+//task 3
+console.log(greeting +" "+ name)
